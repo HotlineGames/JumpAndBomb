fa7aeb2ab6e96cb3d342e5ef731fd31e8c79f534
Julian Klauser <julian.klauser@deutsche-anwaltshotline.de>
2017-07-07 20:12:08 +0200

    Bowser jumping + bugfixes.

--- Assets/BowserJumpController.cs
+++ Assets/BowserJumpController.cs
@@ -1,46 +1,65 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
 public class BowserJumpController : MonoBehaviour
 {
 	private Vector3 _startPosition;
 
 	const int Magnitude = 7;
-	private Vector3 _direction = new Vector3(-1 * Magnitude, 1 * Magnitude, 0 * Magnitude);
+	private Vector3 _direction = new Vector3(-1, 1, 0);
 
 	// Use this for initialization
 	void Start()
 	{
-		var transformComponent = this.GetComponent<Transform>();
+		var transformComponent = GetComponent<Transform>();
 		_startPosition = transformComponent.localPosition;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+
 	}
 
-	void Jump()
+	void ResetForces()
 	{
 		var rigidbodyComponent = GetComponent<Rigidbody>();
 
-		rigidbodyComponent.AddForce(_direction, ForceMode.Impulse);
+		rigidbodyComponent.velocity = new Vector3(0f, 0f, 0f);
+		rigidbodyComponent.angularVelocity = new Vector3(0f, 0f, 0f);
 	}
 
 	void OnCollisionEnter(Collision collision)
 	{
 		var isGroundCollision = collision.gameObject.layer == LayerMask.NameToLayer("Terrain");
 
-		Debug.Log("Collision! " + (isGroundCollision ? "Ground" : ""));
+		//Debug.Log("Collision! " + (isGroundCollision ? "Ground" : ""));
 
 		if (isGroundCollision)
 		{
 			Jump();
-			ChangeDirection();
 		}
 	}
 
-	private void ChangeDirection()
+	private void Jump()
+	{
+		var rigidbodyComponent = GetComponent<Rigidbody>();
+
+		ResetForces();
+
+		ChangeDirectionX();
+		//ChangeDirectionY();
+
+		rigidbodyComponent.AddForce(_direction * Magnitude, ForceMode.Impulse);
+	}
+
+	private void ChangeDirectionX()
 	{
 		_direction.x = -_direction.x;
+	}
+
+	private void ChangeDirectionY()
+	{
+		_direction.y = -_direction.y;
 	}
 }
