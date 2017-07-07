c95acef6cb065e7ef566d555eaee6ee8e9b95e27
TimHofmann <tim.hofmann@deutsche-anwaltshotline.de>
2017-07-07 21:09:47 +0200

    Animation test

---
+++ Assets/JumpAndBomb/NotGoombaController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.JumpAndBomb
+{
+	public class NotGoombaController : MonoBehaviour, IReversable
+	{
+		private Animator anim;
+		public float Speed;
+		public void Reverse()
+		{
+			Speed  *= -1;
+			transform.Rotate(new Vector3(0,1,0), 180);
+		}
+		
+		// Use this for initialization
+		void Start ()
+		{
+			anim = GetComponent<Animator>();
+			anim.SetBool("move", true);
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+			Vector3 newPosition = transform.position;
+			GetComponentInChildren<Rigidbody>().MovePosition(newPosition + new Vector3(-Speed, 0));
+			
+
+		}
+
+		void OnAnimatorMove()
+		{
+			
+		}
+
+		void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.CompareTag("Kill"))
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		void OnCollisionEnter(Collision collision)
+		{
+			var isTerrain = collision.gameObject.layer == LayerMask.NameToLayer("Terrain");
+
+			if (isTerrain)
+			{
+				ContactPoint contact = collision.contacts[0];
+				if (contact.normal.y == -1)
+				{
+					anim.SetBool("move", false);
+					
+				}
+			}
+		}
+	}
+
+	public interface IReversable
+	{
+		void Reverse();
+	}
+}
---
+++ Assets/JumpAndBomb/WalkBackFromWall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.JumpAndBomb
+{
+	public class WalkBackFromWall : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void OnCollisionEnter(Collision collision)
+		{
+			var isTerrain = collision.gameObject.layer == LayerMask.NameToLayer("Terrain");
+
+			if (isTerrain)
+			{
+				ContactPoint contact = collision.contacts[0];
+				if (contact.normal.y == 0.0f)
+					GetComponent<IReversable>().Reverse();
+			}
+		}
+
+	}
+}
