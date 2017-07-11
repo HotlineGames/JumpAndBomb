b37bebbebb1100ed38fa1f5069143a52d56f8b39
Aris Nourbakhsh <aris.nourbakhsh@deutsche-anwaltshotline.de>
2017-07-11 20:10:33 +0200

    Added new movement and breaking

--- Assets/GameObjects/Player/PlayerController.cs
+++ Assets/GameObjects/Player/PlayerController.cs
@@ -1,58 +1,77 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading;
-using System.Timers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-	public float Speed;
-	public int Life;
-	public float MaxSpeedX;
-	public float MaxSpeedY;
-	public bool IsInvul;
-	public float JetPackFuel;
 	public const float DefaultJetPackFuel = 2;
 
 	//Vector3 _playerMovementDirection;
 
 	private Rigidbody _rigidBody;
+	public bool IsInvul;
+	public float JetPackFuel;
+	public int Life;
+	public float MaxSpeedX;
+	public float MaxSpeedY;
+	public float Speed;
+	public float JumpForce;
 
 	// Use this for initialization
-	void Start()
+	private void Start()
 	{
 		_rigidBody = GetComponent<Rigidbody>();
 		ChangeColor(Color.black);
 	}
 
 	// Update is called once per frame
-	void Update()
+	private void Update()
 	{
-		Vector2 input = GetInput();
+		var input = GetInput();
 
-		if (Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon)
+		var anyInput = Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon;
+
+		var velocity = _rigidBody.velocity;
+		if (anyInput)
 		{
-			// always move along the camera forward as it is the direction that it being aimed at
-			/*Vector3 desiredMove = cam.transform.forward * input.y + cam.transform.right * input.x;
-			desiredMove = Vector3.ProjectOnPlane(desiredMove, Vector3.up).normalized;*/
-
-			Vector3 desiredMove = new Vector3(input.x, input.y);
+			var desiredMove = new Vector3(input.x, input.y);
 
 			desiredMove = desiredMove * Speed;
 
-			if (Math.Abs(_rigidBody.velocity.x) < MaxSpeedX || (desiredMove.x > 0 && _rigidBody.velocity.x < 0) || (desiredMove.x < 0 && _rigidBody.velocity.x > 0))
-				_rigidBody.AddForce(new Vector3(desiredMove.x, 0), ForceMode.VelocityChange);
+			var isBelowMaxHorizontalSpeed = Math.Abs(velocity.x) < MaxSpeedX;
+
+			var isAgainstCurrentDirection = desiredMove.x * velocity.x < -Mathf.Epsilon;
+
+			if (isBelowMaxHorizontalSpeed || isAgainstCurrentDirection)
+					_rigidBody.velocity = velocity + new Vector3(desiredMove.x * Speed, 0);
+				
 
 			if (desiredMove.y > 0)
 				if (JetPackFuel > 0.0f)
 				{
-					_rigidBody.AddForce(new Vector3(0, desiredMove.y), ForceMode.VelocityChange);
+					_rigidBody.AddForce(new Vector3(0, desiredMove.y * JumpForce), ForceMode.VelocityChange);
 					JetPackFuel = JetPackFuel - 0.1f;
 				}
+		}
+		else if(Mathf.Abs(velocity.x) > Mathf.Epsilon)
+		{
+			var f = 0.1f;
+
+			var antiDirection = Mathf.Sign(velocity.x) * -1;
+
+			var newVelocity = velocity + new Vector3(f * antiDirection, 0);
+
+			var wouldChangeDirection = velocity.x * newVelocity.x < -Mathf.Epsilon;
+			if (wouldChangeDirection)
+			{
+				_rigidBody.velocity = new Vector3(0, velocity.y);
+			}
+			else
+			{
+				_rigidBody.velocity = newVelocity;
+			}
+			
 		}
 	}
 
@@ -63,49 +82,35 @@ public class PlayerController : MonoBehaviour
 
 	private static Vector2 GetInput()
 	{
-		Vector2 input = new Vector2
+		var input = new Vector2
 		{
-			x = Mathf.Clamp(Input.GetAxis("Horizontal"), -1 , 1),
-			y = Input.GetAxis("Vertical")
+			x = Math.Sign(Input.GetAxis("Horizontal")),
+			y = Math.Sign(Input.GetAxis("Vertical"))
 		};
 		return input;
 	}
 
-	private float GetAbsolute(float value)
-	{
-		if (value > Mathf.Epsilon)
-		{
-			return 1;
-		}
-		if(value < -Mathf.Epsilon)
-		{
-			return -1;
-		}
-		return 0;
-	}
 
-	void ChangeLayer(string layer)
+	private void ChangeLayer(string layer)
 	{
 		gameObject.layer = LayerMask.NameToLayer(layer);
 
 		foreach (Transform child in gameObject.transform)
-		{
 			child.gameObject.layer = LayerMask.NameToLayer(layer);
-		}
 	}
 
-	void ChangeColor(Color color)
+	private void ChangeColor(Color color)
 	{
 		gameObject.GetComponent<MeshRenderer>().sharedMaterial.color = color;
 	}
 
-	IEnumerator Invul()
+	private IEnumerator Invul()
 	{
 		Life -= 1;
 		if (Life <= 0)
 		{
 			ChangeColor(Color.red);
-			SceneManager.LoadScene(("GameOver"));
+			SceneManager.LoadScene("GameOver");
 		}
 		IsInvul = true;
 		ChangeLayer("Invuln");
@@ -116,9 +121,9 @@ public class PlayerController : MonoBehaviour
 		IsInvul = false;
 	}
 
-	IEnumerator Blink(float interval)
+	private IEnumerator Blink(float interval)
 	{
-		for (int i = 0; i < 20; i++)
+		for (var i = 0; i < 20; i++)
 		{
 			ChangeColor(Color.black);
 			yield return new WaitForSeconds(interval / 2);
@@ -128,7 +133,7 @@ public class PlayerController : MonoBehaviour
 	}
 
 
-	void OnCollisionEnter(Collision collision)
+	private void OnCollisionEnter(Collision collision)
 	{
 		var isEnemy = collision.gameObject.CompareTag("Monster");
 
